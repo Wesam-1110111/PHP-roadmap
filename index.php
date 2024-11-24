534c78f3955a3c2d05e84ed557b9887052fc52f7
Wesam <mrw202065@gmail.com>
2024-11-24 19:29:05 +0200

    Upload the test php file

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+// ************  1  *************
+
+/*
+echo 'Hello world form PHP';
+echo 
+
+'<br>';
+print 'print command';
+*/
+
+// ************  2  *************
+
+/*
+echo gettype(2);
+echo "<br>";
+echo gettype(2.5);
+echo "<br>";
+echo gettype(true);
+echo "<br>";
+echo gettype(True);
+echo "<br>";
+echo gettype('el');
+echo "<br>";
+echo gettype('1');
+echo "<br>";
+echo gettype('w');
+echo "<br>";
+echo gettype("wesam");
+echo "<br>";
+echo gettype("2");
+echo "<br>";
+echo gettype(['hello', '2']);
+echo "<br>";
+// echo gettype();
+*/
+
+
+// ******************  3  ********************
+
+
+echo <<<"unorderlist"
+    <ul>
+        <li>3th</li>
+        <li>2ed</li>
+        <li>1st</li>
+    </ul>
+unorderlist;
