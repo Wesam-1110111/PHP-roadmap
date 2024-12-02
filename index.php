e79e4f59df928c34e96356c9d003e705475e74a4
Wesam-1110111 <mrw202065@gmail.com>
2024-12-02 19:46:25 +0200

    New lessons

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ function print_title($n, $title){
 		echo "<br>";
 		
 	}
-	
+
 	echo "<h3>********************  $n  ********************</h3>";
 	echo "<h3>* * * * * * * $title * * * * * * *</h3>";
 	echo "<h3>****************************************</h3>";
@@ -25,3 +25,31 @@ function print_title($n, $title){
 
 print_title(1, 'If, Elseif and Else');
 
+
+echo <<< "form"
+	<form method="POST">
+		<input type="text" name="input" placeholder="Enter to print" required>
+		<button type="submit">Submit</button>
+	</form>
+form;
+
+
+// if ($_POST != false){
+// 	echo $_POST['input'];
+// }
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+	// echo $_POST['input'];
+	if ($_POST['input'] == 0){
+		echo "Sorry, you can not enter zero.";
+	}
+	else{
+		echo $_POST['input'];
+	}
+}
+
+
+//  **************  2  ****************
+
+
+print_title(2, "Advanced if, elseif and else");
