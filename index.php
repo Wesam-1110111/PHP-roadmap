7d8f2dc69361a67a3d4944c7b887c27c151ba5a8
Wesam-1110111 <mrw202065@gmail.com>
2024-12-23 14:19:41 +0200

    String Functions part 2

--- index.php
+++ index.php
@@ -18,3 +18,49 @@ echo 'You can access to item/char in a string like you do with Array.<br>';
 
 
 echo '<br>Example:<br><br><br><code>$name = \'Wesam\';<br>echo $name[3]; // = a</code>';
+
+
+
+//  ********************  2  **********************
+
+
+
+print_title(2, 'String Functions 1');
+
+
+echo '- lcfirst() --> lower case first: only the first letter will convert.<br>';
+echo '- ucfirst() --> upper case first: only the first letter will convert.<br>';
+echo '- strtolower() --> string to lower: the hole string will convert.<br>';
+echo '- strtoupper() --> string to upper: the hole string will convert.<br>';
+echo '- ucwords() --> upper case words: only the first letter of every word will convert.<br>';
+
+
+
+//  *******************  3  ********************
+
+
+
+print_title(3, 'String Functions 2');
+
+
+echo '';
+
+
+
+
+//  *******************  4  ********************
+
+
+
+print_title(4, 'String Functions 3');
+
+
+
+
+echo '- chunk_slpit(String[require], Length[optional], End[optional]) --> Split the string as you want.<br>';
+echo '- strlen(String[require]) --> Return the string length.<br>';
+echo '- str_slpit(String[require], Length[optional]) --> Split the string as you want and return it as Array.<br>';
+
+
+
+
